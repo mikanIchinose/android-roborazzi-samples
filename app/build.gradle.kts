e576cb79d647cda27cac979cb56876e27f334b28
Ichinose Yoshihiro <maruisansmai@gmail.com>
2024-10-06 19:09:48 +0900

    deps: add roborazzi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.roborazzi)
 }
 
 android {
@@ -60,4 +61,7 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.junit4)
     testImplementation(libs.junit)
     testImplementation(libs.robolectric)
+    testImplementation(libs.roborazzi)
+    testImplementation(libs.roborazziCompose)
+    testImplementation(libs.roborazziJunitRule)
 }
